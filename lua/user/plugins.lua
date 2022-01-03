217e4742c9e3daacc894d3a973e4696d9673d2cb
John <handuy099@gmail.com>
2022-01-03 05:16:29 -0800

    [Windows]: remove junk plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,8 +44,6 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use "9mm/vim-closer"
-  use { "iamcco/markdown-preview.nvim", run = "cd app && yarn install", cmd = 'MarkdownPreview' }
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff
   use "kyazdani42/nvim-web-devicons"
