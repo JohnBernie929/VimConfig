4560f4fa6a28238265de5c86ca345b83104b5424
John Bernie <handuy099@gmail.com>
2022-01-04 22:44:55 -0800

    [Windows]: autoformat null-ls

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -24,4 +24,6 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+
+  autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
 ]]
