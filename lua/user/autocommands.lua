aaa22dac8b99ff51c69fc3f59437a6d30b51fea6
John Bernie <handuy099@gmail.com>
2022-01-06 17:20:47 +0700

    [Windows]: remove autoformat

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -24,6 +24,4 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
-
-  autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()
 ]]
