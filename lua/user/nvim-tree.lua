337a6f26543ef35bbb3e76df355e57880847c5c1
DavidH <handuy099@gmail.com>
2023-05-06 20:40:55 +0700

    fix nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -11,8 +11,22 @@ end
 local icons = require "user.icons"
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set("n", "l", api.node.open.edit, opts "Open")
+  vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Close Directory")
+  vim.keymap.set("n", "v", api.node.open.vertical, opts "Open: Vertical Split")
+end
 
 nvim_tree.setup {
+  on_attach = on_attach,
   hijack_directories = {
     enable = false,
   },
@@ -126,14 +140,6 @@ nvim_tree.setup {
     hide_root_folder = false,
     side = "left",
     -- auto_resize = true,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
     number = false,
     relativenumber = false,
   },
