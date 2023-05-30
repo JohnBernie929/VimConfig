c71fc58124b3664f94dd1267761c4025abbbe1f7
DavidH <handuy099@gmail.com>
2023-05-30 15:06:13 +0700

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -123,25 +123,7 @@ lazy.setup({
   { "lvimuser/lsp-inlayhints.nvim", lazy = true },
   { "RRethy/vim-illuminate", event = "User FileOpened" },
   { "SmiteshP/nvim-navic", event = "User FileOpened" },
-  {
-    "Exafunction/codeium.vim",
-    confg = function()
-      local opts = { expr = true, silent = true, noremap = true }
-
-      vim.keymap.set("i", "<M-a>", function()
-        return vim.fn["codeium#Accept"]()
-      end, opts)
-      vim.keymap.set("i", "<M-.>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, opts)
-      vim.keymap.set("i", "<M-,>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, opts)
-      vim.keymap.set("i", "<M-x>", function()
-        return vim.fn["codeium#Clear"]()
-      end, opts)
-    end,
-  },
+  -- { "Exafunction/codeium.vim" },
 
   -- Telescope
   { "nvim-telescope/telescope.nvim" },
