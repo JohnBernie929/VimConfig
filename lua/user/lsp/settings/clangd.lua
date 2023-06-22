d9f7ba735613ab12c110c0cefd49a1372552a908
David Hán <handuy099@gmail.com>
2023-06-22 20:17:28 +0700

    update

--- lua/user/lsp/settings/clangd.lua
+++ lua/user/lsp/settings/clangd.lua
@@ -1,12 +1,3 @@
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
 
 return {
-  cmd = { "C:\\MinGW\\bin\\clangd.exe" },
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-  root_dir = function()
-    return vim.loop.cwd()
-  end,
 }
