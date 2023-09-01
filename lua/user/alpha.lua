be2dcfccfdc31fbca39f60922032d30c2465cebe
Akatsuki <handuy099@gmail.com>
2023-09-01 18:38:05 +0700

    P

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -37,7 +37,7 @@ local function footer()
   -- local fortune = handle:read("*a")
   -- handle:close()
   -- return fortune
-  return "chrisatmachine.com"
+  return "One more Thing"
 end
 
 dashboard.section.footer.val = footer()
