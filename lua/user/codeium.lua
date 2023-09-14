b6c63d8d2ec3725882d788bbc72bf98d32d287fe
Akatsuki <handuy099@gmail.com>
2023-09-14 14:19:48 +0700

    change M to C

--- lua/user/codeium.lua
+++ lua/user/codeium.lua
@@ -1,14 +1,14 @@
 local opts = { expr = true, silent = true, noremap = true }
 
-vim.keymap.set("i", "<M-a>", function()
+vim.keymap.set("i", "<C-a>", function()
   return vim.fn["codeium#Accept"]()
 end, opts)
-vim.keymap.set("i", "<M-.>", function()
+vim.keymap.set("i", "<C-.>", function()
   return vim.fn["codeium#CycleCompletions"](1)
 end, opts)
-vim.keymap.set("i", "<M-,>", function()
+vim.keymap.set("i", "<C-,>", function()
   return vim.fn["codeium#CycleCompletions"](-1)
 end, opts)
-vim.keymap.set("i", "<M-x>", function()
+vim.keymap.set("i", "<C-x>", function()
   return vim.fn["codeium#Clear"]()
 end, opts)
