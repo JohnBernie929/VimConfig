e30c8e436860644b32d2be021e0227766cda0c39
Akatsuki <handuy099@gmail.com>
2023-10-28 19:21:59 +0700

    update

--- lua/user/codeium.lua
+++ lua/user/codeium.lua
@@ -1,14 +1,14 @@
 local opts = { expr = true, silent = true, noremap = true }
 
-vim.keymap.set("i", "<C-a>", function()
+vim.keymap.set("i", "<C-M-a>", function()
   return vim.fn["codeium#Accept"]()
 end, opts)
-vim.keymap.set("i", "<C-.>", function()
+vim.keymap.set("i", "<C-M-.>", function()
   return vim.fn["codeium#CycleCompletions"](1)
 end, opts)
-vim.keymap.set("i", "<C-,>", function()
+vim.keymap.set("i", "<C-M-,>", function()
   return vim.fn["codeium#CycleCompletions"](-1)
 end, opts)
-vim.keymap.set("i", "<C-x>", function()
+vim.keymap.set("i", "<C-M-x>", function()
   return vim.fn["codeium#Clear"]()
 end, opts)
