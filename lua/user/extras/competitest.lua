6f00ce4a967b9160a10c6d0be7ff5eaa1b62c292
Akatsuki <handuy099@gmail.com>
2025-02-11 12:44:30 +0700

    update

--- lua/user/extras/competitest.lua
+++ lua/user/extras/competitest.lua
@@ -9,11 +9,11 @@ function M.config()
     maximum_time = 2000,
     testcases_use_single_file = true,
     compile_command = {
-      c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT).exe" } },
-      cpp = { exec = "g++", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT).exe" } },
+      c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT).bin" } },
+      cpp = { exec = "g++", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT).bin" } },
     },
     run_command = {
-      cpp = { exec = "./$(FNOEXT).exe" },
+      cpp = { exec = "./$(FNOEXT).bin" },
     },
   }
 
